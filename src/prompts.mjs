e1670ae40e3ea4dd8072d0d932f491de218e16e5
rarelyafk <redcaryellowbus@gmail.com>
2023-09-05 20:35:08 -0400

    prompts local module

---
+++ src/prompts.mjs
@@ -0,0 +1,23 @@
+import enquirer from 'enquirer';
+
+const { AutoComplete, StringPrompt, Toggle, MultiSelect } = enquirer;
+
+const acPrompt = async (msg, choices, init) => {
+  const initial = init ? choices.indexOf(init) : null;
+  const message = init ? `${msg} [default: "${init}"]` : msg;
+  // console.log({ initial, message, init, msg });
+  const prompt = new AutoComplete({ message, choices, initial, limit: 10 });
+  return await prompt.run().then(res => res).catch(console.error);
+}
+
+const strPrompt = async message => {
+  const prompt = new StringPrompt({ message });
+  return await prompt.run().then(res => res).catch(console.error);
+}
+
+export {
+  acPrompt,
+  strPrompt,
+  Toggle,
+  MultiSelect,
+};
