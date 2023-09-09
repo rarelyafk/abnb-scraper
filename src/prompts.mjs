43109d4d39c5ce9d2befbf240bb109564bea0883
rarelyafk <redcaryellowbus@gmail.com>
2023-09-09 15:03:21 -0400

    export StringPrompt, remove console log

--- src/prompts.mjs
+++ src/prompts.mjs
@@ -1,12 +1,14 @@
 import enquirer from 'enquirer';
 
+
 const { AutoComplete, StringPrompt, Toggle, MultiSelect } = enquirer;
 
 const acPrompt = async (msg, choices, init) => {
   const initial = init ? choices.indexOf(init) : null;
   const message = init ? `${msg} [default: "${init}"]` : msg;
-  // console.log({ initial, message, init, msg });
+
   const prompt = new AutoComplete({ message, choices, initial, limit: 10 });
+
   return await prompt.run().then(res => res).catch(console.error);
 }
 
@@ -15,9 +17,12 @@ const strPrompt = async message => {
   return await prompt.run().then(res => res).catch(console.error);
 }
 
+
 export {
   acPrompt,
   strPrompt,
+  StringPrompt,
   Toggle,
   MultiSelect,
 };
+
