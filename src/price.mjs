85c4453c9cd0289f3e772811088356980f2bba5b
rarelyafk <redcaryellowbus@gmail.com>
2023-09-09 15:05:06 -0400

    exptracted price to local module, added simple validator to StringPrompt

---
+++ src/price.mjs
@@ -0,0 +1,41 @@
+import { StringPrompt } from './prompts.mjs';
+
+const minPricePrompt = new StringPrompt({
+  type: 'string',
+  name: 'minPrice',
+  message: 'Minimum Price',
+  validate: (value) => {
+    if (Number(value) <= 0)
+      return prompt.styles.danger('numbers less than 0 not allowed...');
+    if (Number(value) >= 9999)
+      return prompt.styles.danger('numbers more than 9999 not allowed...');
+    return true;
+  },
+});
+
+const maxPricePrompt = new StringPrompt({
+  type: 'string',
+  name: 'maxPrice',
+  message: 'Maximum Price',
+  validate: (value) => {
+    if (Number(value) <= 0)
+      return prompt.styles.danger('numbers less than 0 not allowed...');
+    if (Number(value) >= 9999)
+      return prompt.styles.danger('numbers more than 9999 not allowed...');
+    return true;
+  },
+});
+
+const getMinPrice = async () => (
+  await minPricePrompt.run()
+    .then(minPrice => minPrice)
+    .catch(console.error)
+);
+
+const getMaxPrice = async () => (
+  await maxPricePrompt.run()
+    .then(minPrice => minPrice)
+    .catch(console.error)
+);
+
+export { getMinPrice, getMaxPrice };
