4c40959dc71135e18eceff1d07ffc87fe669c0fa
rarelyafk <redcaryellowbus@gmail.com>
2023-09-09 15:05:57 -0400

    renamed func to export, added comments to non-obvious lines, needs work...

--- src/getABnBs.mjs
+++ src/getABnBs.mjs
@@ -2,21 +2,24 @@ import getBrowserInstance from "./browserInstance.mjs";
 import makeUrl from './makeUrl.mjs';
 import loadPage from './loadPage.mjs';
 
-const getAirbnbHotels = async (location, monthly, checkIn, checkOut, priceMin, priceMax) => {
+const getABnBs = async (location, monthly, checkIn, checkOut, priceMin, priceMax) => {
   const url = makeUrl(location, monthly, checkIn, checkOut, priceMin, priceMax);
 
   const { page, closeBrowser } = await getBrowserInstance();
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-  await page.waitForSelector('#site-content');
+  // await page.waitForSelector('#site-content');
+
   const waitAndClick = async selector => {
     if (await page.$(selector) !== null) {
       await page.waitForSelector(selector);
       await page.click(selector);
     }
   };
-  waitAndClick('.c1pmjrqe');
-  waitAndClick('.canm9xs');
+
+  await page.waitForSelector('#site-content'); // main site content
+  // await waitAndClick('.c1pmjrqe');             // display total price (width1)
+  await waitAndClick('.canm9xs');              // display total price (width2)
 
   let cnt = 1;
   console.log(`Page ${cnt}`);
@@ -33,4 +36,4 @@ const getAirbnbHotels = async (location, monthly, checkIn, checkOut, priceMin, p
   await closeBrowser();
 };
 
-export default getAirbnbHotels;
+export default getABnBs;
