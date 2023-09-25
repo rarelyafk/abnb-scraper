c52c7d80313e762588931c30345e88fb3b549412
rarelyafk <redcaryellowbus@gmail.com>
2023-09-24 21:35:34 -0400

    remove log, comment cruft

--- src/query/makeUrl.mjs
+++ src/query/makeUrl.mjs
@@ -10,19 +10,6 @@ const makeUrl = (
   priceMax,
   amenities,
 ) => {
-  // console.log(
-  //   'makeUrl: ', {
-  //     location,
-  //     isMonthly,
-  //     months,
-  //     checkIn,
-  //     checkOut,
-  //     priceMin,
-  //     priceMax,
-  //     amenities,
-  //   }
-  // );
-  
   let queryParams = '';
 
   queryParams += 'tab_id=home_tab';
@@ -62,7 +49,8 @@ const makeUrl = (
 
   queryParams += '&adults=1';
   queryParams += '&source=structured_search_input_header';
-  queryParams += '&search_type=autocomplete_click'; // queryParams += '&search_type=filter_change';
+  queryParams += '&search_type=autocomplete_click';
+  // queryParams += '&search_type=filter_change';
 
   if (priceMin)
     queryParams += `&price_min=${priceMin}`;
@@ -79,52 +67,6 @@ const makeUrl = (
   const host = 'https://www.airbnb.com';
   const url = `${host}/s/${location}/homes?${queryParams}`;
   return url;
-
-  // &refinement_paths%5B%5D=%2Fhomes
-  // &flexible_trip_lengths%5B%5D=one_week
-  // &monthly_start_date=2023-10-01
-  // &monthly_length=3
-  // &price_filter_input_type=0
-  // &price_filter_num_nights=5
-  // &channel=EXPLORE
-  // &query=Los%20Angeles%2C%20CA
-  // &place_id=ChIJE9on3F3HwoAR9AhGJW_fL-I
-  // &date_picker_type=calendar
-  // &checkin=2023-09-15
-  // &checkout=2023-09-20
-  // &adults=1
-  // &source=structured_search_input_header
-  // &search_type=filter_change
-  // &price_min=500
-  // &room_types%5B%5D=Entire%20home%2Fapt
-  // &amenities%5B%5D=30
-
-
-  // generic //////////////////////////////////////////////////////////////////
-  // queryParams += '&adults=1&channel=EXPLORE&tab_id=home_tab';
-  // queryParams += '&price_filter_input_type=1';              // ?
-  // queryParams += `&${encodeURIComponent('refinement_paths[]')}=${encodeURIComponent('/homes')}`;
-  // queryParams += `&${encodeURIComponent('room_types[]')}=${encodeURIComponent('Entire home/apt')}`;
-  // queryParams += `&${encodeURIComponent('amenities[]')}=4`;
-  // multiple amenities, just add
-  // queryParams += `&${encodeURIComponent('amenities[]')}=7`;
-
-  // date /////////////////////////////////////////////////////////////////////
-  // queryParams += `&checkIn=${checkIn}`;
-  // queryParams += `&checkOut=${checkOut}`;
-  // const dateArr = checkIn.split('-');
-  // queryParams += `&monthly_start_date=${dateArr[0]}-${dateArr[1]}-01`;
-  // queryParams += monthly
-  //   ? '&date_picker_type=monthly_stay&monthly_length=1&price_filter_num_nights=30' // MONTHLY
-  //   : '&date_picker_type=calendar';                                                // DAILY
-  // price filters ///////////////////////////////////////////////////////////
-  // queryParams += `&price_min=${priceMin}`;
-  // queryParams += `&price_max=${priceMax}`;
-
-  // const url = `${host}/s/${location}/homes?${queryParams}`;
-  // console.log({ url });
-
-  // return url;
 };
 
 export default makeUrl;
