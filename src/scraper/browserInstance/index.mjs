e2835afdcba0d6728d00fdabdb50e1b81a35204c
rarelyafk <redcaryellowbus@gmail.com>
2023-09-25 12:02:42 -0400

    remove comment

--- src/scraper/browserInstance/index.mjs
+++ src/scraper/browserInstance/index.mjs
@@ -11,7 +11,6 @@ const getBrowserInstance = async () => {
     executablePath: executablePath(),
   });
 
-  // const page = await browser.newPage();
   const page = (await browser.pages())[0];
   await page.setViewport({
     width: 1600,
