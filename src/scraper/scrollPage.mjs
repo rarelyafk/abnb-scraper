1a0617df930f7ae8111be62ae4f427c130f5ae6e
rarelyafk <redcaryellowbus@gmail.com>
2023-09-25 11:15:02 -0400

    remove comment cruft

--- src/scraper/scrollPage.mjs
+++ src/scraper/scrollPage.mjs
@@ -12,18 +12,6 @@ async function scrollPage(page) {
       documentHeight = document.body.scrollHeight
     }
   });
-  // await page.evaluate(() => new Promise((resolve) => {
-  //   let scrollTop = -1;
-  //   const interval = setInterval(() => {
-  //     window.scrollBy(0, 100);
-  //     if(document.documentElement.scrollTop !== scrollTop) {
-  //       scrollTop = document.documentElement.scrollTop;
-  //       return;
-  //     }
-  //     clearInterval(interval);
-  //     resolve();
-  //   }, 10);
-  // }));
 };
 
 export default scrollPage;
