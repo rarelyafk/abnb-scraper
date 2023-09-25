cec31ae90e77a7157d40e06147af34fba7707c05
rarelyafk <redcaryellowbus@gmail.com>
2023-09-25 12:07:00 -0400

    setTimeout's without clearTimeout's = BAD PRACTICE

--- src/scraper/scrollPage.mjs
+++ src/scraper/scrollPage.mjs
@@ -5,6 +5,7 @@ const scrollPage = async (page) => {
 
     while (documentHeight > scrollPosition) {
       window.scrollBy(0, documentHeight);
+      // NOTE: setTimeout's without clearTimeout's = BAD PRACTICE
       await new Promise(resolve => setTimeout(resolve, 1000));
       scrollPosition = documentHeight;
       documentHeight = document.body.scrollHeight;
