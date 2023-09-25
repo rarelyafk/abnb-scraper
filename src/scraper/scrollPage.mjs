56c1a1111e52e0902840d1d6e25166f0152e20f6
rarelyafk <redcaryellowbus@gmail.com>
2023-09-25 12:05:31 -0400

    formatting, NOTE! setTimeout without clearTimeout BAD PRACTICE

--- src/scraper/scrollPage.mjs
+++ src/scraper/scrollPage.mjs
@@ -1,15 +1,13 @@
-async function scrollPage(page) {
+const scrollPage = async (page) => {
   await page.evaluate(async () => {
-    let scrollPosition = 0
-    let documentHeight = document.body.scrollHeight
+    let scrollPosition = 0;
+    let documentHeight = document.body.scrollHeight;
 
     while (documentHeight > scrollPosition) {
-      window.scrollBy(0, documentHeight)
-      await new Promise(resolve => {
-        setTimeout(resolve, 1000)
-      })
-      scrollPosition = documentHeight
-      documentHeight = document.body.scrollHeight
+      window.scrollBy(0, documentHeight);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      scrollPosition = documentHeight;
+      documentHeight = document.body.scrollHeight;
     }
   });
 };
