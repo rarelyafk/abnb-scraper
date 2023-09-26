4b315441067322b2c347e56f931d7596e0143811
rarelyafk <redcaryellowbus@gmail.com>
2023-09-25 21:03:39 -0400

    destructuring assignment for conciseness

--- src/scraper/scrollPage.mjs
+++ src/scraper/scrollPage.mjs
@@ -1,14 +1,12 @@
 const scrollPage = async (page) => {
   await page.evaluate(async () => {
-    let scrollPosition = 0;
-    let documentHeight = document.body.scrollHeight;
+    let [scrollPosition, documentHeight] = [0, document.body.scrollHeight];
 
     while (documentHeight > scrollPosition) {
       window.scrollBy(0, documentHeight);
       // NOTE: setTimeout's without clearTimeout's = BAD PRACTICE
       await new Promise(resolve => setTimeout(resolve, 1000));
-      scrollPosition = documentHeight;
-      documentHeight = document.body.scrollHeight;
+      [scrollPosition, documentHeight] = [documentHeight, document.body.scrollHeight];
     }
   });
 };
